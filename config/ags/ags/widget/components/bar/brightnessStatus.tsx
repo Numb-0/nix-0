235e8a3d7939a9fbefcbe70bbaaafdba89b09705
Numb-0 <cosimofalla@gmail.com>
2025-02-09 17:01:05 +0100

    made status buttons active

--- config/ags/ags/widget/components/bar/brightnessStatus.tsx
+++ config/ags/ags/widget/components/bar/brightnessStatus.tsx
@@ -5,7 +5,7 @@ import Brightness from "../../../services/Brightness"
 export default function BrightnessStatus() {
     const brightness = new Brightness();
 
-    return <box cssClasses={["brightness"]} spacing={2}>
+    return <box onScroll={(_,__,dy)=> dy < 0 ? brightness.value += 0.01 : brightness.value += -0.01} cssClasses={["brightness"]} spacing={2}>
             <image iconName={bind(brightness, "iconName")} />
             <label label={bind(brightness, "value").as(p => `${Math.round(p * 100)}%`)}/>
     </box>
