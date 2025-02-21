a07c9858159b793cd257cc163e8e35315c78e891
Numb-0 <cosimofalla@gmail.com>
2025-02-21 01:12:59 +0100

    added btop to home

--- hosts/nixos/config.nix
+++ hosts/nixos/config.nix
@@ -89,17 +89,6 @@ in
       aapt
     ];
   };
-  
-  programs.btop = {
-    enable = true;
-    settings = {
-      # color_theme is set by stylix
-      theme_background = false;
-      proc_gradient = false;
-      rounded_corners = false;
-      presets = "cpu:0:braille,mem:0:braille,proc:0:braille,net:0:braille,disks:0:braille";
-    };
-  };
 
   programs = {
     adb.enable = true;
--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -34,6 +34,18 @@ in
     userEmail = "${gitEmail}";
   };
 
+  # Btop
+  programs.btop = {
+    enable = true;
+    settings = {
+      # color_theme is set by stylix
+      theme_background = false;
+      proc_gradient = false;
+      rounded_corners = false;
+      presets = "cpu:0:braille,mem:0:braille,proc:0:braille,net:0:braille,disks:0:braille";
+    };
+  };
+
   # Create XDG Dirs
   xdg = {
     enable = true;
