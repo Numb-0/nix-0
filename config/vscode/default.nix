3470c130af7037d5a2af224c1e12cea0b16fe4c9
Numb-0 <cosimofalla@gmail.com>
2025-02-25 20:14:54 +0100

    changed vscode configs

--- config/vscode/default.nix
+++ config/vscode/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   osConfig,
   ...
 }:
@@ -10,42 +9,33 @@ let
   }.${osConfig.style.scheme} or "Default Dark Modern";
   icontheme = {
     "gruvbox" = "gruvbox-material-icon-theme";
+    "catppuccin" = "catppuccin-macchiato";
   }.${osConfig.style.scheme} or "material-icon-theme";
 in
 {
   programs.vscode = {
     enable = true;
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          # TODO make package vscode-extensions gruvbox material icon theme
-          bbenoist.nix
-          jnoortheen.nix-ide
-          catppuccin.catppuccin-vsc
-          pkief.material-icon-theme
-          jdinhlife.gruvbox
-          esbenp.prettier-vscode
-        ];
-        userSettings = {
-          nix = {
-            enableLanguageServer = true;
-            serverPath = "nil";
-          };
-          workbench = {
-            colorTheme = colortheme;
-            iconTheme = icontheme;
-          };
-          editor = {
-            fontFamily = "JetBrainsMono Nerd Font Mono";
-          };
-          "[scss]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-          "[typescriptreact]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
-          };
-        };
-      };
-    };
   };
+  xdg.configFile."Code/User/settings.json".text = ''
+  "workbench": {
+    "colorTheme": ${colortheme},
+    "iconTheme": ${icontheme}
+  }
+  "editor": {
+    "fontFamily": "JetBrainsMono Nerd Font Mono"
+  },
+  "[scss]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "nix.enableLanguageServer": true,
+  "nix.serverPath": "nil"
+  "nix.serverSettings": {
+    "nil": {
+      "formatting": { "command": ["nixfmt"] }
+    }
+  }
+  '';
 }
